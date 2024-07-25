4d5e038b1797babd97981188246589f1c335672a
Wilmer  Restrepo <107453564+Wilo92@users.noreply.github.com>
2024-07-24 20:58:30 -0500

    Add files via upload

---
+++ vehiculos/base.php
@@ -0,0 +1,13 @@
+<?php
+//SE INCLUYEN LA PLANTILLA DEL HEADER */ 
+include "./includes/template/header.php"
+?>
+
+<main class="contenedor seccion">
+    <h1>Titulo Página</h1>
+</main>
+
+<?php
+//SE INCLUYEN LA PLANTILLA DEL FOOTER */ 
+include "./includes/template/footer.php"
+?>
